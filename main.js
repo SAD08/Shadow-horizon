1103e4bc2b004a94da4ca6e6d81c2a0cf9874978
SAD08 <sevrukova799@gmail.com>
2025-11-04 17:36:03 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-main.js
+const archiveToggle = document.querySelector('.archive-toggle');
+const archivePanel = document.querySelector('.archive-panel');
+
+archiveToggle.addEventListener('click', () => {
+  archivePanel.classList.toggle('active');
+});
+
+const libraryToggle = document.querySelector('.library-toggle');
+const libraryPanel = document.querySelector('.library-panel');
+
+libraryToggle.addEventListener('click', () => {
+  libraryPanel.classList.toggle('active');
+});
+
+const LojesToggle = document.querySelector('.Lojes-toggle');
+const LojesPanel = document.querySelector('.Lojes-panel');
+
+LojesToggle.addEventListener('click', () => {
+  LojesPanel.classList.toggle('active');
+});
