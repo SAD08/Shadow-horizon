fedc7c9e3560b8963f94fdb8ac50b3dca2dc6a11
SAD08 <sevrukova799@gmail.com>
2025-11-04 17:24:31 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-const archiveToggle = document.querySelector('.archive-toggle');
-const archivePanel = document.querySelector('.archive-panel');
-
-archiveToggle.addEventListener('click', () => {
-  archivePanel.classList.toggle('active');
-});
-
-const libraryToggle = document.querySelector('.library-toggle');
-const libraryPanel = document.querySelector('.library-panel');
-
-libraryToggle.addEventListener('click', () => {
-  libraryPanel.classList.toggle('active');
-});
+main.js
